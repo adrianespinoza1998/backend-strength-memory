c7522a6de7dab2e4022acb19c516938c06e2afad
Adrian Espinoza <adrianespinozaarevalo@gmail.com>
2023-04-07 19:39:40 -0400

    add middlewares

--- index.ts
+++ index.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import { config } from "dotenv";
 import cors from "cors";
+import queryParser from "express-query-int";
+import fileUpload from "express-fileupload";
 
 config();
 
@@ -10,6 +12,14 @@ const port = process.env.PORT || 3000;
 app.use(express.static("public"));
 app.use(cors());
 app.use(express.json());
+app.use(queryParser());
+app.use(
+  fileUpload({
+    useTempFiles: true,
+    tempFileDir: "/tmp/",
+    createParentPath: true,
+  })
+);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
