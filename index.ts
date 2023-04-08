7535370f79b7853fe751034b6b9151c02ae68d3b
Adrian Espinoza <adrianespinozaarevalo@gmail.com>
2023-04-08 19:38:18 -0400

    add db config

--- index.ts
+++ index.ts
@@ -1,13 +1,21 @@
 import express from "express";
-import { config } from "dotenv";
+import "dotenv/config";
 import cors from "cors";
 import queryParser from "express-query-int";
 import fileUpload from "express-fileupload";
-
-config();
+import { sequelize } from "./src/database/dbConfig";
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+(async () => {
+  try {
+    await sequelize.authenticate();
+    console.log("Base de datos en linea");
+  } catch (error) {
+    console.warn("Error en la base de datos: ", error);
+  }
+})();
 
 app.use(express.static("public"));
 app.use(cors());
